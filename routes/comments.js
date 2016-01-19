19f7cdefc257ce2b2abb64cff8914864f7b967b1
Robbie McClellan <robbiemcclellan14@gmail.com>
2016-01-19 13:44:47 -0800

    changed small aspects on comment route

--- routes/comments.js
+++ routes/comments.js
@@ -1,21 +1,24 @@
 import express from 'express';
-import Comment from '../models/Comment'
-import {authenticate, passChange} from '../util/authMiddleware';
+
+// Import Schema To Interact With Mongo
+import Comment from '../models/Comment';
+
+// Integrate For Authentication
+import { authenticate, passChange } from '../util/authMiddleware';
 
 const router = express.Router();
 
-// add new comment to comment
+// Create Comment And To A Another Comment
 router.post('/:id/newcomment', (req, res) => {
   Comment.create(req.body, (err, newComment) => {
-    if (err) return res.status(400).send(err);
-    Comment.findByIdAndUpdate(req.params.id, {$push: {comments: newComment}}, (err) => {
-      if (err) return res.status(400).send(err);
+    if(err) return res.status(400).send(err);
+    Comment.findByIdAndUpdate(req.params.id, { $push: { comments: newComment }}, (err) => {
+      if(err) return res.status(400).send(err);
       Comment.findById(req.params.id, (err, comment) => {
-        if (err) return res.status(400).send(err);
-        res.send(comment);
+        res.status(err ? 400:200).send(err || comment);
       }).populate('comments');
-    })
-  })
+    });
+  });
 });
 
 export default router;
