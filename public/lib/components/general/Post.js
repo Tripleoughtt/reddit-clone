3d9afc2a2523ba6754cbec953f25ab96e2b4c8b8
Tripleoughtt <richieg72@gmail.com>
2016-01-24 16:22:39 -0800

    removed console logs

--- public/lib/components/general/Post.js
+++ public/lib/components/general/Post.js
@@ -10,15 +10,12 @@ class Post extends React.Component{
     let params = 'post/' + this.props.data._id
     let snippets = this.props.data.body.split(' ').slice(0, 30).join(' ');
     let authorDisplayName = this.props.data.author.name;
-    console.log(this.props.data.author.name)
     let author;
     if(authorDisplayName){
       author = authorDisplayName;
     } else {
       author = this.props.data.author.username;
     }
-    console.log(this.props.data)
-    console.log(author)
     return(
       <div className="postComponent">
         <h1><Link to={params}>{this.props.data.title}</Link></h1>
